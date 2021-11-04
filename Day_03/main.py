ce08abdfb26d8d202eca5733fdef799001f0c76f
JanKnapen <44979696+JanKnapen@users.noreply.github.com>
2021-11-04 09:48:58 +0100

    Day_03 improved part 2

--- Day_03/main.py
+++ Day_03/main.py
@@ -15,44 +15,33 @@ def get_result1(input_value):
     return dis
 
 
+def get_neighbors_sum(grid, x, y):
+    neighbors_sum = 0
+    for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        neighbors_sum += grid[x+dx, y+dy]
+    return neighbors_sum
+
+
 def get_result2(input_value):
-    n = 0
-    while (2*n+1)**2 < input_value:
-        n += 1
+    max_spiral = 0
+    while (2*max_spiral+1)**2 < input_value:
+        max_spiral += 1
 
     grid = defaultdict(int)
     grid[0, 0] = 1
-    for i in range(1, n):
-        x, y = i, -i+1
-        grid[x, y] = grid[x - 1, y - 1] + grid[x - 1, y] + grid[x - 1, y + 1] + grid[x, y - 1] + grid[x, y + 1] + grid[
-            x + 1, y - 1] + grid[x + 1, y] + grid[x + 1, y + 1]
+
+    for spiral in range(1, max_spiral):
+        width = 2*spiral+1
+        x, y = spiral, 1 - spiral
+        grid[x, y] = get_neighbors_sum(grid, x, y)
         if grid[x, y] > input_value:
             return grid[x, y]
-        width = 2*i+1
-        for j in range(width-2):
-            y += 1
-            grid[x, y] = grid[x - 1, y - 1] + grid[x - 1, y] + grid[x - 1, y + 1] + grid[x, y - 1] + grid[x, y + 1] + \
-                         grid[x + 1, y - 1] + grid[x + 1, y] + grid[x + 1, y + 1]
-            if grid[x, y] > input_value:
-                return grid[x, y]
-        for j in range(width-1):
-            x -= 1
-            grid[x, y] = grid[x - 1, y - 1] + grid[x - 1, y] + grid[x - 1, y + 1] + grid[x, y - 1] + grid[x, y + 1] + \
-                         grid[x + 1, y - 1] + grid[x + 1, y] + grid[x + 1, y + 1]
-            if grid[x, y] > input_value:
-                return grid[x, y]
-        for j in range(width-1):
-            y -= 1
-            grid[x, y] = grid[x - 1, y - 1] + grid[x - 1, y] + grid[x - 1, y + 1] + grid[x, y - 1] + grid[x, y + 1] + \
-                         grid[x + 1, y - 1] + grid[x + 1, y] + grid[x + 1, y + 1]
-            if grid[x, y] > input_value:
-                return grid[x, y]
-        for j in range(width-1):
-            x += 1
-            grid[x, y] = grid[x - 1, y - 1] + grid[x - 1, y] + grid[x - 1, y + 1] + grid[x, y - 1] + grid[x, y + 1] + \
-                         grid[x + 1, y - 1] + grid[x + 1, y] + grid[x + 1, y + 1]
-            if grid[x, y] > input_value:
-                return grid[x, y]
+        for dx, dy, num_steps in [(0, 1, width-2), (-1, 0, width-1), (0, -1, width-1), (1, 0, width-1)]:
+            for i in range(num_steps):
+                x, y = x+dx, y+dy
+                grid[x, y] = get_neighbors_sum(grid, x, y)
+                if grid[x, y] > input_value:
+                    return grid[x, y]
     return -1
 
 
